ff72309fae0b0d3647fd27ef4a16b9ca2d38c96a
shiwenjun <371608656@qq.com>
2018-04-11 17:28:54 +0800

    修改

--- where.php
+++ where.php
@@ -19,7 +19,7 @@ class WhereServer{
         }
     }
 
-    private function arr2str($arr){
+    private function arr2str($arr,$type='and'){
         if (isset($arr[0])) {
 			if($arr[0] == 'and' || $arr[0] == 'or'){
 				$type = $arr[0];
@@ -29,7 +29,7 @@ class WhereServer{
 				return $this->CreateSql($arr);
 			}
         } else { 
-            return $this->CreateSql($arr);
+			return $this->buildAndCondition($type, $arr);
         }
 
     }
@@ -67,10 +67,10 @@ class WhereServer{
         $parts = [];
         foreach ($operands as $key=>$operand) {
 			if(!is_numeric($key)){
-				$operand = $this->arr2str([$key=>$operand]);
+				$operand = $this->CreateSql([$key=>$operand]);
 			}
             if (is_array($operand)) {
-                $operand = $this->arr2str($operand);
+                $operand = $this->arr2str($operand,$operator);
             }
            
             if ($operand !== '') {
@@ -88,7 +88,7 @@ class WhereServer{
 
 //$arr = ["and",'platform_id'=>1,"platform_id=1",["between", "status", 1, 3],['or',[">=", "platform_id", 1],["between", "status", 1, 3],['and',["like", "platform_id", 1],["between", "status", 1, 3]],['or',[">=", "platform_id", 1],["between", "status", 1, 3]]],["and", ["not in", "platform_id", [1,2,3,4]],["between", "status", 1, 3]]];
 //$arr = ['platform_id'=>1,'platform_ids'=>1];
-$arr = '["and", {
+/*$arr = '["and", {
 			"user_id": 313
 		},
 		["or", ["and", ["in", "status", [1, 3, 4, 5]],
@@ -100,9 +100,11 @@ $arr = '["and", {
 				[">=", "roll_out_time", 1517826431]
 			]
 		]
-	]';
+	]';*/
+	$arr = '["and",["<","type_id",997],{"platform_id":1,"user_id":467}]';
 	$arr = json_decode($arr,true);
 $where = new WhereServer();
 echo $where->getWhereStr($arr);
+
 
 ?>
